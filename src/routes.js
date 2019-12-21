e7750463c837f07559bd08301f5abc6ec6a3b5c3
caiopoyares <capoyares@gmail.com>
2019-12-21 11:25:47 -0300

    New model and controller for Ads

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,20 @@
 const express = require("express");
-const routes = express.Router();
+const router = express.Router();
 
-const UserController = require("./app/controllers/UserController");
-const SessionController = require("./app/controllers/SessionController");
+const AuthMiddleware = require("./app/middlewares/auth");
 
-routes.post('/users', UserController.store);
-routes.post('/sessions', SessionController.store);
+const controllers = require('./app/controllers');
 
-module.exports = routes;
+router.post('/users', controllers.UserController.store);
+router.post('/sessions', controllers.SessionController.store);
+
+router.use(AuthMiddleware);
+
+// Add Routes
+router.get('/ads', controllers.AdController.index)
+router.get('/ads/:id', controllers.AdController.show)
+router.post('/ads', controllers.AdController.store)
+router.put('/ads/:id', controllers.AdController.update)
+router.delete('/ads/:id', controllers.AdController.destroy)
+
+module.exports = router;
